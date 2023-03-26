1627b144c4e43c70ec975c31b6382819019c08ca
HP <sreekanta.csd@gmail.com>
2023-03-26 21:04:09 +0600

    login part completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import app from "./firebase.init";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useState } from "react";
@@ -10,55 +10,91 @@ import { useState } from "react";
 const auth = getAuth(app);
 
 function App() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [error, setError]= useState('')
+  const [password, setPassword] = useState("");
 
-  const handleEmailBlur= (event) => {
+  const handleEmailBlur = (event) => {
     setEmail(event.target.value);
   };
 
-  const handlePasswordBlur= (event)=>{
-    setPassword(event.target.value)
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+     
+      event.stopPropagation();
+      return;
+    }
+   
+    if(!/(?=.*?[#?!@$%^&*-])/.test(password)){
+      setError('Password should contain one special character')
+      return;
+    }
+    
+    
 
-  }
-const handleSubmit=(event)=>{
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((result)=>{
-    const user = result.user;
-    console.log(user)
-  })
-  .catch((error)=>{
-    console.error(error)
-  })
-  event.preventDefault();
-  // console.log('working', email, password)
-}
+    setValidated(true);
+    setError('')
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    event.preventDefault();
+    // console.log('working', email, password)
+  };
 
- 
   return (
     <div className="App">
       <div className="registration w-50 mx-auto mt-3">
         <h1 className="text-primary">Please Register!!</h1>
-      <Form  onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              onBlur={handleEmailBlur}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onBlur={handlePasswordBlur}
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid Password.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <h5 className="text-danger">{error}</h5>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
   );
